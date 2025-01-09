486929ded68641673296145c86b8214c87362701
Vic Rubba <crazedVic@users.noreply.github.com>
2025-01-09 15:36:41 -0700

    asdf

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,23 +17,33 @@ class DatabaseSeeder extends Seeder
     {
         // Create 5 test users
         // Create an admin user
-        $user = User::create([
+        User::create([
             'email' => 'ed@ed.com',
-            'password' => bcrypt('password'),
+            'password' => bcrypt('passwd99'),
             'name' => 'Ed',
             'is_admin' => true,
         ]);
         
-        // Seed the remaining 4 users
-        User::factory(8)->create();
+        $user = User::create([
+            'email' => 'bob@bob.com',
+            'password' => bcrypt('passwd99'),
+            'name' => 'Bob',
+            'is_admin' => false,
+        ]);
 
+
+        // Seed the remaining 4 users
+        $users = User::factory(9)->create()->prepend($user);
+        
+        // Create a new user for Bob
+        
         // Create 10 broadcasts with dates in the last 30 days
         Broadcast::factory(20)->create()->each(function ($broadcast) {
             $broadcast->created_at = Carbon::now()->subDays(rand(0, 30));
             $broadcast->save();
         });
 
-        $users = User::all();
+        //$users = User::all();
         $broadcasts = Broadcast::all();
 
         // Randomly mark some broadcasts as read or deleted by some users
