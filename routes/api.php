260cdf36bbe8f1ee6f2e54b9f28f4ad0661c4ff5
Vic Rubba <crazedVic@users.noreply.github.com>
2025-01-13 13:30:15 -0700

    new api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserBroadcastController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Public routes
+Route::get('/user/token', function() {
+    $user = \App\Models\User::find(2);
+    $token = $user->createToken('test-token')->plainTextToken;
+    return response()->json([
+        'token' => $token
+    ]);
+});
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    // Broadcast routes
+    Route::get('/broadcasts', [UserBroadcastController::class, 'index']);
+    Route::put('/broadcasts/{broadcast}/state', [UserBroadcastController::class, 'updateState']);
+});
